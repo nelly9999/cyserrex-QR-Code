043b9ee0fa1b8f0d03a61607f7bf6fc409cc1d60
Adny Nizomi <war_twister@yahoo.co.id>
2017-08-08 07:03:25 +0800

    indexnya

---
+++ admin/index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+	<title>Administrator</title>
+
+	<link rel="stylesheet" href="../css/qr_admin.css">
+	<link rel="stylesheet" href="../easyui/themes/ui-sunny/easyui.css">
+	<link rel="stylesheet" href="../easyui/themes/icon.css">
+	<script type="text/javascript" src="../easyui/jquery.min.js"></script>	
+	<script type="text/javascript" src="../easyui/jquery.easyui.min.js"></script>	
+</head>
+<body>
+
+<nav>
+	<ul>
+        <li class="pegawai"><a href="index.php?p=pegawai">Pegawai</a></li>
+        <li class="presensi"><a href="index.php?p=presensi">Presensi</a></li>
+		<li class="laporan"><a href="index.php?p=laporan">Laporan</a></li>
+		<li class="statistik"><a href="index.php?p=statistik">Statistik</a></li>
+		<li class="qr_spesial"><a href="index.php?p=qr_spesial">QR Spesial</a></li>
+		<li class="pengaturan"><a href="index.php?p=pengaturan">Pengaturan</a></li>
+        <li class="logout"><a href="index.php?p=logout">Logout</a></li>
+	</ul>
+</nav>
+ 
+<div id="konten">
+	<?php
+	$pages_dir = '../admin';
+	if(!empty($_GET['p'])){
+		$pages = scandir($pages_dir, 0);
+		unset($pages[0], $pages[1]);
+
+		$p = $_GET['p'];
+		if(in_array($p.'.php', $pages)){
+			include($pages_dir.'/'.$p.'.php');
+		} else {
+			echo 'Halaman tidak ditemukan! :(';
+		}
+	} else {
+		include($pages_dir.'/pegawai.php');
+	}
+	?>
+</div>
+ 
+</body>
+</html>
